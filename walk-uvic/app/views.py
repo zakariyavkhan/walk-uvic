3632a92ff7e8534017b03e3b8d490fad56807a7e
Zakariya Khan <zakariyavkhan@gmail.com>
2023-01-05 16:14:58 -0800

    add files

--- walk-uvic/app/views.py
+++ walk-uvic/app/views.py
@@ -12,11 +12,15 @@ home = Blueprint('home', __name__)
 
 @home.route('/', methods=['GET', 'POST'])
 def home_page():
+    # draw, save map to map.html
+
     form = NodeForm(nodes=db.Query.all())
     if request.method == 'POST' and form.validate_on_submit():
         origin_node = db.Query.get(form.origin.data)
         destination_node = db.Query.get(form.destination.data)
 
+        # .save('templates/map.html')
+        # redirect
         #pass node ids to osmnx
         #draw route and add to map.html
 

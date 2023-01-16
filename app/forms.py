40ec271c9b66849c3d03d26c47726caca3c61f6f
Zakariya Khan <zakariyavkhan@gmail.com>
2023-01-15 19:40:37 -0800

    display folium map

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField
 
 class NodeForm(FlaskForm):
-    origin = SelectField('Origin',
-                         coerce=int)
-    destination = SelectField('Destination',
-                              coerce=int)
+    origin = SelectField('Origin')
+    destination = SelectField('Destination')
     submit = SubmitField(label='Go')
 
     def __init__(self, nodes=None):
         super().__init__()
         if nodes:
-            self.origin.choices = nodes
-            self.destination.choices = nodes
+            self.origin.choices = [node.name for node in nodes]
+            self.destination.choices = [node.name for node in nodes]
